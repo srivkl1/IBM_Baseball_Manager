f9899a46416885accba261ac0a62a4ab3219ba1f
CLi012 <cli997895@gmail.com>
2026-04-14 19:39:07 -0400

    Testing out pybaseball

--- data.py
+++ data.py
@@ -1,1 +1,15 @@
 # Baseball data here and so called API's from Statcast, fangraphs, MLB blahblahblah
+
+# Verifying panda exists
+import pandas as pd
+print(pd.__version__)
+
+
+from pybaseball import statcast
+from pybaseball import playerid_lookup
+from pybaseball import  statcast_pitcher
+
+df = playerid_lookup('schwarber', 'kyle')
+print(df)
+
+
