df9caa58c39f58691b8af48e0b99bec94837334a
CLi012 <cli997895@gmail.com>
2026-04-14 19:55:03 -0400

    Testing current schedule for teams

--- data.py
+++ data.py
@@ -9,7 +9,13 @@ from pybaseball import statcast
 from pybaseball import playerid_lookup
 from pybaseball import  statcast_pitcher
 
+from pybaseball import schedule_and_record
+
 df = playerid_lookup('schwarber', 'kyle')
 print(df)
+
+# Data for schedule for 2026 Dodgers
+df_2 = schedule_and_record(2026, 'LAD')  # team = Dodgers
+print(df_2)
 
 
